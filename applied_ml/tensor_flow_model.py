7c59e54268982e86f9c39169c3c0db4fd37431cf
Your Name <your_email@whatever.com>
2017-03-24 13:51:30 +0300

    upd

--- applied_ml/tensor_flow_model.py
+++ applied_ml/tensor_flow_model.py
@@ -57,6 +57,7 @@ def train_neural_network(x):
 		sess.run(tf.global_variables_initializer())
 		
 		for epoch in range(hm_epochs):
+		
 			epoch_loss = 0
 			for _ in range(int(mnist.train.num_examples / batch_size)):
 				epoch_x, epoch_y = mnist.train.next_batch(batch_size)
