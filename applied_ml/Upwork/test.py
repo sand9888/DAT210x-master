ba1e1a5871886b6143978711c13e087ec6e36485
Your Name <your_email@whatever.com>
2017-05-04 23:26:26 +0300

    upd2fg

--- applied_ml/Upwork/test.py
+++ applied_ml/Upwork/test.py
@@ -6,20 +6,13 @@ import os
 root_dir = os.path.abspath('../Upwork')
 filename = 'disagg.csv'
 df_scource = pd.read_csv(os.path.join(root_dir, filename))
-# df_scource.Month = pd.to_datetime(df_scource.Month,  yearfirst=True, format='%m/%d/%y %H:%M')
+df_scource.Month = pd.to_datetime(df_scource.Month,  yearfirst=True, format='%m/%d/%y %H:%M')
 # print(df.Month.dt.year)
 
 # remain data where AppId equals 0
 df = df_scource[df_scource['AppId'] == 0].reset_index()
 df_non_raw = df_scource[df_scource.AppId != 0].reset_index()
 df_non_raw['quantileId'] = np.nan
-
-print(df.columns)
-print(df.ix[0, 'UUID'])
-
-for i in range(2,6):
-	# print(df_non_raw.ix[i, 'Month'])
-	print(df.ix[i, 'Month'])
 
 # dividing data by percentiles as function parameter
 def quantile_number(quant_num = 5):
@@ -41,10 +34,14 @@ def quantile_number(quant_num = 5):
 				df_q.loc[:, 'quantileId'] = i
 				df_final = df_final.append(df_q)
 	
-	for i, date_month  in enumerate(df_final['Month']):
+	for i, (date_month, quant) in enumerate(zip(df_final['Month'], df_final['quantileId'])):
+		df_non_raw.quantileId[df_non_raw['Month'] == date_month] == df_final.ix[i, 'quantileId']
+		
+	print(df_non_raw.head(10))
+	'''	df_n
 		for j, quant_id in enumerate(df_non_raw['quantileId']):
-			# print(i, date_month, j, quant_id)
-			if (i > 1) and (j > 1) and (df_final.ix[i, 'Month'] == df_non_raw.ix[j, 'Month']) and (df_final.ix[i, 'UUID'] == df_non_raw.ix[j, 'UUID']):
+			print(i, date_month, j, quant_id)
+			if (df_final.ix[i, 'Month'] == df_non_raw.ix[j, 'Month']) and (df_final.ix[i, 'UUID'] == df_non_raw.ix[j, 'UUID']):
 				df_non_raw.ix[j, 'quantileId'] = df_final.ix[i, 'quantileId']
 				
 		
@@ -55,9 +52,9 @@ def quantile_number(quant_num = 5):
 		print('Quantile ', i, ': \n', 'Average Consumption:', quant['Consumption'].mean(), '\n', 'Median Consumption: ',
 			  quant['Consumption'].median())
 	df_final['Consumption_mean_percent'] = df_final['Consumption'] / df_non_raw['Consumption'].mean()
-	df_final['Consumption_median_percent'] = df_final['Consumption'] / df_non_raw['Consumption'].median()
+	df_final['Consumption_median_percent'] = df_final['Consumption'] / df_non_raw['Consumption'].median()'''
 	return df_final
 
 df_final = quantile_number()
 
-# computing descriptive statistic'''
+# computing descriptive statistic
